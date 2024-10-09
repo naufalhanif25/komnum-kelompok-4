271b9e4614d2d798cc3f62120498748c5aa3c080
Naufal Hanif <naufal.hanif2525@gmail.com>
2024-10-09 13:57:40 +0700

    main

---
+++ komnum_kelompok_4.py
@@ -0,0 +1,22 @@
+import numpy as np
+import tkinter as tk
+import matplotlib as plt
+import prettytable as pt
+
+def interface():
+    print("Interface")
+
+def function():
+    print("f(x)")
+
+def iteration_method():
+    print("Metode iterasi")
+
+def table():
+    print("Table")
+
+def plot():
+    print("Plot")
+
+def main():
+    print("Main")

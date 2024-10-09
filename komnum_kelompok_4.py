5a4227503bca5e5e3c31f1875437941011bae5dc
Naufal Hanif <naufal.hanif2525@gmail.com>
2024-10-09 14:50:12 +0700

    update

--- komnum_kelompok_4.py
+++ komnum_kelompok_4.py
@@ -4,7 +4,23 @@ import matplotlib as plt
 import prettytable as pt
 
 def interface():
-    print("Interface")
+    interface = tk.Tk()
+
+    tk.Label(interface, text = "Fungsi f(x)").grid(padx = 12, pady = 12, row = 0)
+
+    entry_1 = tk.Entry(interface, width = 64)
+    entry_1.grid(row = 0, column = 1)
+
+    tk.Label(interface, text = "Error").grid(padx = 12, pady = 0, row = 1)
+    
+    entry_2 = tk.Entry(interface, width = 64)
+    entry_2.grid(row = 1, column = 1)
+
+    interface.geometry("660x400")
+    interface.title("Metode Iterasi")
+    interface.iconbitmap("icon.ico")
+
+    interface.mainloop()
 
 def function():
     print("f(x)")
@@ -19,4 +35,6 @@ def plot():
     print("Plot")
 
 def main():
-    print("Main")
+    interface()
+
+main()

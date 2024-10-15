58c3d87225a4dbd6630bc1e312cfb7e363a5486d
Naufal Hanif <naufal.hanif2525@gmail.com>
2024-10-15 14:24:14 +0700

    update

--- komnum_kelompok_4.py
+++ komnum_kelompok_4.py
@@ -1,7 +1,7 @@
 import numpy as np
 import tkinter as tk
-import matplotlib as plt
-import prettytable as pt
+import matplotlib
+import os
 
 def convert_function(input_func):
     input_func = input_func.replace("^", "**").replace("x", "*x")
@@ -31,50 +31,77 @@ def table():
 def plot():
     print("Plot")
 
-def main():
-    interface = tk.Tk()
+def output_processing():
+    global func_fx , func_gx, first_x, N_iter, error
 
-    tk.Label(interface, text = "INPUT", font = ("Arial", 8, "bold")).grid(padx = 12, pady = 6, row = 0)
+    func_fx = entry_fx.get()
+    func_gx = entry_gx.get()
+    first_x = entry_x1.get()
+    N_iter = entry_n_iter.get()
+    error = entry_error.get()
 
-    tk.Label(interface, text = "Fungsi f(x)").grid(padx = 12, pady = 0, row = 1)
-    tk.Label(interface, text = "Fungsi g(x)").grid(padx = 12, pady = 6, row = 2)
-    tk.Label(interface, text = "X awal").grid(padx = 12, pady = 0, row = 3)
-    tk.Label(interface, text = "Iterasi (N)").grid(padx = 12, pady = 6, row = 4)
-    tk.Label(interface, text = "Error").grid(padx = 12, pady = 0, row = 5)
+    fx = insert_func_value(func_fx, first_x)
+    gx = insert_func_value(func_gx, first_x)
 
-    entry_1 = tk.Entry(interface, width = 64)
-    entry_1.grid(row = 1, column = 1)
+current_dir = os.path.dirname(__file__)
 
-    entry_2 = tk.Entry(interface, width = 64)
-    entry_2.grid(row = 2, column = 1)
+BACKGROUND_COLOR = "#FFFFFF"
+FILL_COLOR = "#F1F2F6"
+BUTTON_COLOR_1 = "#00A8FF"
+BUTTON_COLOR_2 = "#FF4757"
 
-    entry_3 = tk.Entry(interface, width = 64)
-    entry_3.grid(row = 3, column = 1)
+interface = tk.Tk()
+interface.resizable(False, False)
+interface.title("Metode Iterasi")
+interface.iconbitmap(current_dir + "\\icon.ico")
+interface.config(bg = BACKGROUND_COLOR)
 
-    entry_4 = tk.Entry(interface, width = 64)
-    entry_4.grid(row = 4, column = 1)
+input_label = tk.Label(interface, bg = BACKGROUND_COLOR, text = "Input", font = ("Arial", 12, "bold"), anchor = "center")
+input_label.grid(padx = (20, 0), pady = (6, 0), row = 0, column = 0)
 
-    entry_5 = tk.Entry(interface, width = 64)
-    entry_5.grid(row = 5, column = 1)
+func_fx = tk.Label(interface, bg = BACKGROUND_COLOR, text = "Fungsi f(x)", font = ("Arial", 8), anchor = "w")
+func_fx.grid(padx = (20, 6), pady = (6, 0), row = 1, column = 0, sticky = "w")
 
-    tk.Label(interface, text = "\n", font = "Arial 4").grid(padx = 12, pady = 0, row = 6)
+func_gx = tk.Label(interface, bg = BACKGROUND_COLOR, text = "Fungsi g(x)", font = ("Arial", 8), anchor = "w")
+func_gx.grid(padx = (20, 6), pady = (6, 0), row = 2, column = 0, sticky = "w")
 
-    def get_value():
-        func_fx = entry_1.get()
-        func_gx = entry_2.get()
-        first_x = entry_3.get()
-        iter_N = entry_4.get()
-        error = entry_5.get()
+first_x = tk.Label(interface, bg = BACKGROUND_COLOR, text = "x awal", font = ("Arial", 8), anchor = "w")
+first_x.grid(padx = (20, 6), pady = (6, 0), row = 3, column = 0, sticky = "w")
 
-    button = tk.Button(text = "Run", command = get_value)
-    button.grid(padx = 12, pady = 6, row = 7)
+N_iteration = tk.Label(interface, bg = BACKGROUND_COLOR, text = "Iterasi N", font = ("Arial", 8), anchor = "w")
+N_iteration.grid(padx = (20, 6), pady = (6, 0), row = 4, column = 0, sticky = "w")
 
-    tk.Label(interface, text = "OUTPUT", font = ("Arial", 8, "bold")).grid(padx = 12, pady = 6, row = 8)
+error = tk.Label(interface, bg = BACKGROUND_COLOR, text = "Error", font = ("Arial", 8), anchor = "w")
+error.grid(padx = (20, 6), pady = (6, 0), row = 5, column = 0, sticky = "w")
 
-    interface.geometry("500x400")
-    interface.title("Metode Iterasi")
-    interface.iconbitmap("icon.ico")
+entry_fx = tk.Entry(interface, bg = FILL_COLOR, borderwidth = 1, width = 64, relief = "groove")
+entry_fx.grid(padx = (12, 20), pady = (8, 0), row = 1, column = 1, sticky = "w")
 
-    interface.mainloop()
+entry_gx = tk.Entry(interface, bg = FILL_COLOR, borderwidth = 1, width = 64, relief = "groove")
+entry_gx.grid(padx = (12, 20), pady = (8, 0), row = 2, column = 1, sticky = "w")
 
-main()
+entry_x1 = tk.Entry(interface, bg = FILL_COLOR, borderwidth = 1, width = 64, relief = "groove")
+entry_x1.grid(padx = (12, 20), pady = (8, 0), row = 3, column = 1, sticky = "w")
+
+entry_n_iter = tk.Entry(interface, bg = FILL_COLOR, borderwidth = 1, width = 64, relief = "groove")
+entry_n_iter.grid(padx = (12, 20), pady = (8, 0), row = 4, column = 1, sticky = "w")
+
+entry_error = tk.Entry(interface, bg = FILL_COLOR, borderwidth = 1, width = 64, relief = "groove")
+entry_error.grid(padx = (12, 20), pady = (8, 0), row = 5, column = 1, sticky = "w")
+
+run_button = tk.Button(interface, text = "Run", padx = 6, command = output_processing, font = ("Arial", 8, "bold"))
+run_button.config(bg = BUTTON_COLOR_1, fg = BACKGROUND_COLOR, relief  = "groove", cursor = "hand2")
+run_button.grid(padx = (20, 0), pady = (12, 0), row = 6, column = 0)
+
+output_label = tk.Label(interface, bg = BACKGROUND_COLOR, text = "Output", font = ("Arial", 12, "bold"), anchor = "center")
+output_label.grid(padx = (20, 6), pady = (20, 0), row = 7, column = 0)
+
+exit_button = tk.Button(interface, text = "Exit", padx = 6, command = interface.destroy, font = ("Arial", 8, "bold"))
+exit_button.config(bg = BUTTON_COLOR_2, fg = BACKGROUND_COLOR, relief  = "groove", cursor = "hand2")
+exit_button.grid(padx = (20, 0), pady = (6, 20), row = 8, column = 0)
+
+plot_button = tk.Button(interface, text = "Show Plot", padx = 6, command = plot, font = ("Arial", 8, "bold"))
+plot_button.config(bg = BUTTON_COLOR_1, fg = BACKGROUND_COLOR, relief  = "groove", cursor = "hand2")
+plot_button.grid(padx = (0, 0), pady = (6, 20), row = 8, column = 1, sticky = "w")
+
+interface.mainloop()

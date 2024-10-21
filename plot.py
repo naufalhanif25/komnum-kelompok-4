e393eaf3fd3512b1e3bb897bdbe82a2923edbb6b
RahmadHidayatullah <166033080+Ryonn12@users.noreply.github.com>
2024-10-21 11:25:50 +0700

    update plot

--- plot.py
+++ plot.py
@@ -1,13 +1,17 @@
 import matplotlib.pyplot as plt
+import pandas as pd
 
 def plot(x_final, data):
+    #Membuat dataframe
+    df = pd.DataFrame(data, columns=['Iterasi', 'x', 'F(x)', 'G(x)'])
+    
     # Plotting hasil
     plt.figure(figsize=(10, 6))
 
     # Plot nilai x sepanjang iterasi
-    plt.plot(data['Iterasi'], data['x'], label='x', marker='o', color='blue')
-    plt.plot(data['Iterasi'], data['F(x)'], label='F(x)', marker='x', color='green')
-    plt.plot(data['Iterasi'], data['G(x)'], label='G(x)', marker='^', color='orange')
+    plt.plot(df['Iterasi'], df['x'], label='x', marker='o', color='blue')
+    plt.plot(df['Iterasi'], df['F(x)'], label='F(x)', marker='x', color='green')
+    plt.plot(df['Iterasi'], df['G(x)'], label='G(x)', marker='^', color='orange')
 
     # Menambahkan judul dan label
     plt.title('Hasil Iterasi Titik Tetap')
@@ -16,7 +20,5 @@ def plot(x_final, data):
     plt.yscale('linear')
     plt.grid()
     plt.legend()
-    
-    #Menampilkan plot
     plt.show()
     
